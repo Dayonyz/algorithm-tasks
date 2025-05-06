f98995bf91e594539933b8dadadd8e8a01ebe061
Pisklov Denys <denys.pisklov@gmail.com>
2025-05-06 17:00:31 +0300

    Pretty results for array tests

--- tests/Unit/LongestArithmeticSeriesTest.php
+++ tests/Unit/LongestArithmeticSeriesTest.php
@@ -18,8 +18,12 @@ class LongestArithmeticSeriesTest extends TestCase
     public function test_one_series_result()
     {
         $input = [1, 4, 7, 5, 3, 9];
+
         $results = [
-            2 => [ 0 => [1, 3, 5, 7, 9] ]
+            2 =>
+                [
+                    [1, 3, 5, 7, 9]
+                ]
         ];
 
         $this->assertEquals($results, longestArithmeticSeries($input));
@@ -28,9 +32,16 @@ class LongestArithmeticSeriesTest extends TestCase
     public function test_two_series_results_same_length_with_different_inc_and_cross_elements()
     {
         $input = [1, 4, 7, 5, 3, 9, 10, 13];
+
         $results = [
-            2 => [ [1, 3, 5, 7, 9] ],
-            3 => [ [1, 4, 7, 10, 13] ]
+            2 =>
+                [
+                    [1, 3, 5, 7, 9]
+                ],
+            3 =>
+                [
+                    [1, 4, 7, 10, 13]
+                ]
         ];
 
         $this->assertEquals($results, longestArithmeticSeries($input));
@@ -40,9 +51,19 @@ class LongestArithmeticSeriesTest extends TestCase
     {
         $input = [1, 4, 7, 5, 3, 9, 10, 13, 18, 23, 28, 33, 40, 45, 50, 55, 60];
         $results = [
-            2 => [ [1, 3, 5, 7, 9] ],
-            3 => [ [1, 4, 7, 10, 13] ],
-            5 => [ [13, 18, 23, 28, 33], [40, 45, 50, 55, 60]]
+            2 =>
+                [
+                    [1, 3, 5, 7, 9]
+                ],
+            3 =>
+                [
+                    [1, 4, 7, 10, 13]
+                ],
+            5 =>
+                [
+                    [13, 18, 23, 28, 33],
+                    [40, 45, 50, 55, 60]
+                ]
         ];
 
         $this->assertEquals($results, longestArithmeticSeries($input));
